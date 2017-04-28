755ca61cc644fd66401ed490f1b2bf3d5556e163
Shirish Kadam <shirishkadam35@yahoo.com>
2017-04-28 21:49:39 +0530

    Added MySQL database

--- django_adam_qas/settings.py
+++ django_adam_qas/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'django_adam_qas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qas',
+        'USER': 'felix',
+        'HOST': 'localhost',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'static')+'/qas_mysql.cnf',
+        },
     }
 }
 
