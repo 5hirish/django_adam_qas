065bcfbef0a6f14c27f82b3fa6ed2d7d46b1ba05
Shirish Kadam <shirishkadam35@yahoo.com>
2017-06-02 20:16:11 +0530

    Added SQLite

--- django_adam_qas/settings.py
+++ django_adam_qas/settings.py
@@ -74,7 +74,12 @@ WSGI_APPLICATION = 'django_adam_qas.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
